6ae3bcf049ebb35ce893086b5cde0c050a3667af
harasz31 <59858062+harasz31@users.noreply.github.com>
2023-06-14 09:48:02 +0700

    Update Tugas_RPL3.java

--- Tugas_RPL3.java
+++ Tugas_RPL3.java
@@ -12,7 +12,6 @@ public class ComputerServiceCost {
         System.out.print("Is the customer willing to drop off and pick up? (Y/N): ");
         String dropOffPickUp = scanner.next();
         
-        // Perform the case statement for computer amount
         int baseFee = 0;
         int additionalFee = 0;
         if (computerAmount == 1 || computerAmount == 2) {
@@ -26,20 +25,17 @@ public class ComputerServiceCost {
             additionalFee = 10;
         }
         
-        // Check if service time is not in business hours and double the base fee
+       
         if (serviceTime.equalsIgnoreCase("N")) {
             baseFee *= 2;
         }
         
-        // Check if customer is willing to drop off and pick up and reduce the total base fee to one-half
+        
         if (dropOffPickUp.equalsIgnoreCase("Y")) {
             baseFee /= 2;
         }
-        
-        // Calculate the total fee
         int totalFee = baseFee + additionalFee * computerAmount;
         
-        // Print the results
         System.out.println("Base fee: $" + baseFee);
         System.out.println("Additional fee: $" + additionalFee + " per peripheral");
         System.out.println("Total fee: $" + totalFee);
